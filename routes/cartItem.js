ae5aad903adb2e5473a87ee9e148f2561053abba
Kartik <kartikkaushik1820@gmail.com>
2024-08-13 20:30:32 +0530

    fix the cart button error in menu.ejs

--- routes/cartItem.js
+++ routes/cartItem.js
@@ -8,6 +8,7 @@ const ExpressError = require("../utils/expressError.js");
 const { validateRecipie, isLoggedIn, isOwner } = require("../middleware.js");
 const { saveredirectUrl } = require("../middleware.js");
 const { use } = require("passport");
+const { recipieSchema } = require("../schema.js");
 
 function checkLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
@@ -22,6 +23,43 @@ function checkLoggedIn(req, res, next) {
   }
 }
 
+router.post("/addToCart/:id", saveredirectUrl, isLoggedIn, async (req, res) => {
+  const { id: recipieId } = req.params;
+  const recipie = await Recipe.findById(recipieId);
+  const user = await User.findById(req.user._id);
+  const CartItemExist = await CartItem.findOne({ name: recipie.name });
+  if (
+    CartItemExist &&
+    CartItemExist.owner._id.equals(res.locals.currUser._id)
+  ) {
+    quantity = CartItemExist.quantity + 1;
+    totalPrice = CartItemExist.price * quantity;
+    updatedCartItem = await CartItem.findByIdAndUpdate(CartItemExist.id, {
+      quantity: quantity,
+      price: totalPrice,
+    });
+    // console.log(updatedCartItem);
+    req.flash("success", "Item Added to Cart!");
+    res.redirect("/recipies");
+  } else {
+    try {
+      const newCartItem = new CartItem({
+        name: recipie.name,
+        price: recipie.price,
+        image: recipie.image,
+        quantity: 1,
+        owner: req.user._id,
+      });
+      // console.log(newCartItem);
+      await newCartItem.save();
+      req.flash("success", "Item Added to Cart!");
+      res.redirect("/recipies");
+    } catch (error) {
+      res.status(500).json({ message: "Error adding recipe to cart" });
+    }
+  }
+});
+
 router.get("/add-to-cart/:id", (req, res) => {
   const { id } = req.params;
   req.flash("success", "Added to Cart!");
@@ -33,10 +71,12 @@ router.post(
   saveredirectUrl,
   checkLoggedIn,
   async (req, res) => {
+    console.log(req.body);
     const { recipeId } = req.body;
     const recipie = await Recipe.findById(recipeId);
     // const user = await User.findById(req.user._id);
     // console.log(user);
+
     const CartItemExist = await CartItem.findOne({ name: recipie.name });
     if (
       CartItemExist &&
@@ -48,7 +88,7 @@ router.post(
         quantity: quantity,
         price: totalPrice,
       });
-      console.log(updatedCartItem);
+      // console.log(updatedCartItem);
     } else {
       try {
         const newCartItem = new CartItem({
@@ -58,7 +98,7 @@ router.post(
           quantity: 1,
           owner: req.user._id,
         });
-        console.log(newCartItem);
+        // console.log(newCartItem);
         await newCartItem.save();
         res.json({ message: "Recipe added to cart" });
       } catch (error) {
@@ -71,7 +111,7 @@ router.post(
 router.delete("/cartItems/:id", async (req, res) => {
   let { id } = req.params;
   const deletedCartItem = await CartItem.findByIdAndDelete(id);
-  console.log(deletedCartItem);
+  // console.log(deletedCartItem);
   res.redirect("/cartItems");
 });
 
@@ -82,29 +122,14 @@ router.get(
     let userId = await User.find({ username: req.session.passport.user });
     loginUser = userId[0]._id;
     let cartItems = await CartItem.find({ owner: loginUser });
-    console.log(cartItems);
+    // console.log(cartItems);
     res.render("cart/cart.ejs", { cartItems });
   })
 );
 
-router.get("/cartItems/placeOrder", (req, res) => {
-  res.redirect("/");
+router.get("/cartItems/placeOrder", isLoggedIn, (req, res) => {
+  req.flash("success", "Order Place Suceessfully!");
+  res.redirect("/recipies");
 });
 
-// router.post("/cart", async (req, res) => {
-//   const cart = req.body.cart;
-//   console.log(cart);
-// });
-
-// router.post("/send-cart", async (req, res) => {
-//     const recipieIds = req.body.cart;
-//     const recipiePromises = recipieIds.map(async (recipieId) => {
-//       return Recipie.findById(recipieId);
-//     });
-
-//     const recipies = await Promise.all(recipiePromises);
-
-//     console.log(recipies);
-//     res.render("cart/cart.ejs", { recipies });
-//   });
 module.exports = router;
